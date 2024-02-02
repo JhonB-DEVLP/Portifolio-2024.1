e9e97f9a98775eae8b1013fba99d64eb612ef77d
JhonB-DEVLP <jhonesbonifacio_18@outlook.com>
2024-02-01 21:58:17 -0300

    Portifólio completo

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
 /* Show menu */
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close');
 
 /* Menu Show*/
 /* Validar se a constante existe */
+    if(navToggle) {
+        navToggle.addEventListener('click', () => {
+            navMenu.classList.add('show-menu');
+    })
+}
 
 /* Menu Hidden */
 /* Validar se a constante existe */
+if(navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+})
+}
 
 /* Remover menu do celular */
+const navLink = document.querySelectorAll('.nav__link');
+
+    function linkAction() {
+        const navMenu = document.getElementById('nav-menu');
+        // when we click on each nav__link, we remove the show-menu class
+        navMenu.classList.remove('show-menu');
+}
+
+navLink.forEach((n) => n.addEventListener('click', linkAction));
+
 
 /* Scroll sections active Link */
 
