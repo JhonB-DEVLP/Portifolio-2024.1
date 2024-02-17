735c66048f6d2b8bbcbb2247a5cd4303f32e5bdf
JhonB-DEVLP <jhonesbonifacio_18@outlook.com>
2024-02-17 13:28:24 -0300

    Atualização

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 /* Show menu */
 const navMenu = document.getElementById('nav-menu'),
-navToggle = document.getElementById('nav-toggle'),
-navClose = document.getElementById('nav-close');
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close');
 
 /* Menu Show*/
 /* Validar se a constante existe */
-    if(navToggle) {
-        navToggle.addEventListener('click', () => {
-            navMenu.classList.add('show-menu');
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
     })
 }
 
 /* Menu Hidden */
 /* Validar se a constante existe */
-if(navClose) {
+if (navClose) {
     navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu');
-})
+    })
 }
 
 /* Remover menu do celular */
 const navLink = document.querySelectorAll('.nav__link');
 
-    function linkAction() {
-        const navMenu = document.getElementById('nav-menu');
-        // when we click on each nav__link, we remove the show-menu class
-        navMenu.classList.remove('show-menu');
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu');
+    // when we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu');
 }
 
 navLink.forEach((n) => n.addEventListener('click', linkAction));
@@ -37,8 +37,8 @@ navLink.forEach((n) => n.addEventListener('click', linkAction));
 function scrollHeader() {
     const header = document.getElementById('header');
     // when the scroll is greater than 50 viewport height, add the scroll-header class to header tag
-       if(this.scrollY >= 80) header.classList.add('scroll-header');
-       else header.classList.remove('scroll-header');
+    if (this.scrollY >= 80) header.classList.add('scroll-header');
+    else header.classList.remove('scroll-header');
 }
 
 window.addEventListener('scroll', scrollHeader);
@@ -47,8 +47,8 @@ window.addEventListener('scroll', scrollHeader);
 function scrollUp() {
     const scrollUp = document.getElementById('scroll-up');
     // when the scroll is greater than 350 viewport height, add the show-scroll class to scroll-top class
-       if(this.scrollY >= 350) scrollUp.classList.add('show-scroll');
-       else scrollUp.classList.remove('show-scroll');
+    if (this.scrollY >= 350) scrollUp.classList.add('show-scroll');
+    else scrollUp.classList.remove('show-scroll');
 }
 
 window.addEventListener('scroll', scrollUp);
@@ -58,7 +58,7 @@ const tabs = document.querySelectorAll('[data-target]'),
     tabContents = document.querySelectorAll('[data-content]');
 
 tabs.forEach((tab) => {
-    tab.addEventListener('click', () => { 
+    tab.addEventListener('click', () => {
         const target = document.querySelector(tab.dataset.target);
 
         tabContents.forEach((tabContent) => {
@@ -77,58 +77,58 @@ tabs.forEach((tab) => {
 
 /* Formulário de contato */
 const contatoForm = document.getElementById('contato-form'),
-      contatoName = document.getElementById('contato-name'),
-      contatoEmail = document.getElementById('contato-email'),
-      contatoSubject = document.getElementById('contato-subject'),
-      contatoMessage = document.getElementById('contato-message'),
-      mensagemErro = document.getElementById('mensagem-erro');
+    contatoName = document.getElementById('contato-name'),
+    contatoEmail = document.getElementById('contato-email'),
+    contatoSubject = document.getElementById('contato-subject'),
+    contatoMessage = document.getElementById('contato-message'),
+    mensagemErro = document.getElementById('mensagem-erro');
 
-    const enviarEmail = (e) => {
-        e.preventDefault();
+const enviarEmail = (e) => {
+    e.preventDefault();
 
-        // Verificar se o campo a ser preenchido tem algum tipo de valor
-        if (
-            contatoName.value === '' ||
-            contatoEmail.value === '' || 
-            contatoSubject.value === '' || 
-            contatoMessage.value === ''
-          ) {
+    // Verificar se o campo a ser preenchido tem algum tipo de valor
+    if (
+        contatoName.value === '' ||
+        contatoEmail.value === '' ||
+        contatoSubject.value === '' ||
+        contatoMessage.value === ''
+    ) {
 
         // Mensagem de erro
-            mensagemErro.textContent = 'Preencha todos os campos obrigatórios 😉'
-        } 
+        mensagemErro.textContent = 'Preencha todos os campos obrigatórios 😉'
+    }
 
-        else {
-            // serviceID - TemplateID - #form - publickey
-            emailjs.sendForm(
-                'service_rj4nwud',
-                'template_p2ri2vl',
-                '#contato-form',
-                'XAvL7RxhjDLFfUFEm'
-                ).then(() => {
-                    // Show menssage and add color, window + dot to open emoji
-                    mensagemErro.classList.add('color-first');
-                    mensagemErro.textContent = 'Mensagem enviada com sucesso! ✔️';
+    else {
+        // serviceID - TemplateID - #form - publickey
+        emailjs.sendForm(
+            'service_rj4nwud',
+            'template_p2ri2vl',
+            '#contato-form',
+            'XAvL7RxhjDLFfUFEm'
+        ).then(() => {
+            // Show menssage and add color, window + dot to open emoji
+            mensagemErro.classList.add('color-first');
+            mensagemErro.textContent = 'Mensagem enviada com sucesso! ✔️';
 
-                    // Remover mensagem depois de 7 segundos
-                    setTimeout(() => {
-                        mensagemErro.textContent = '';
-                    }, 5000);
-                }, 
-                (error) => {
-                    alert('Oxe! Alguma coisa aconteceu de errado...', error);
-                }
-            );
+            // Remover mensagem depois de 7 segundos
+            setTimeout(() => {
+                mensagemErro.textContent = '';
+            }, 5000);
+        },
+            (error) => {
+                alert('Oxe! Alguma coisa aconteceu de errado...', error);
+            }
+        );
 
-            // limpar campos de entrada
-            contatoName.value = '';
-            contatoEmail.value = '';
-            contatoSubject.value = '';
-            contatoMessage.value = '';
-        }
+        // limpar campos de entrada
+        contatoName.value = '';
+        contatoEmail.value = '';
+        contatoSubject.value = '';
+        contatoMessage.value = '';
+    }
 };
 
-    contatoForm.addEventListener('submit', enviarEmail);
+contatoForm.addEventListener('submit', enviarEmail);
 
 /* Efeito Máscara */
 
